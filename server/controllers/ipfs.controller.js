a8a28404fea1a48e4806e2ca8b4d3e31c116064d
Amir Malek <ahmalekpour1999@gmail.com>
2023-07-04 18:21:27 +0330

    minor changes

--- server/controllers/ipfs.controller.js
+++ server/controllers/ipfs.controller.js
@@ -59,6 +59,10 @@ module.exports = {
         const {
           callback,
           attributes,
+          name,
+          // eslint-disable-next-line camelcase
+          external_url,
+          description,
         } = req.body;
 
         await validateIpfsUpload(req.body);
@@ -96,9 +100,10 @@ module.exports = {
         nft.image = image.id;
         if (req.files.media) nft.media = media.id;
         nft.metadata = {
-          name: req.body.name,
-          external_url: req.body.external_url,
-          description: req.body.description,
+          name,
+          // eslint-disable-next-line camelcase
+          external_url,
+          description,
           attributes,
         };
 
@@ -113,99 +118,99 @@ module.exports = {
       next(e);
     }
   },
-  PostUploadBulk: async (req, res, next) => {
-    try {
-      const NFTFilesDest = './server/storage/nfts';
-      const multerStorage = multer.diskStorage({
-        destination: (_, file, cb) => {
-          fs.mkdirSync(NFTFilesDest, { recursive: true });
-          cb(null, NFTFilesDest);
-        },
-        filename: (_, file, cb) => {
-          cb(null, generateFilename(file.originalname));
-        },
-      });
+  // PostUploadBulk: async (req, res, next) => {
+  //   try {
+  //     const NFTFilesDest = './server/storage/nfts';
+  //     const multerStorage = multer.diskStorage({
+  //       destination: (_, file, cb) => {
+  //         fs.mkdirSync(NFTFilesDest, { recursive: true });
+  //         cb(null, NFTFilesDest);
+  //       },
+  //       filename: (_, file, cb) => {
+  //         cb(null, generateFilename(file.originalname));
+  //       },
+  //     });
 
-      const multerFilter = (_, file, cb) => {
-        const ext = path.extname(file.originalname);
-        if (!allowedMediaExtensions.includes(ext.toLowerCase())) {
-          return cb(new Error(`${ext} files are not supported`));
-        }
+  //     const multerFilter = (_, file, cb) => {
+  //       const ext = path.extname(file.originalname);
+  //       if (!allowedMediaExtensions.includes(ext.toLowerCase())) {
+  //         return cb(new Error(`${ext} files are not supported`));
+  //       }
 
-        cb(null, true);
-      };
+  //       cb(null, true);
+  //     };
 
-      const upload = multer({
-        storage: multerStorage,
-        fileFilter: multerFilter,
-        limits: {
-          fileSize: 100 * 1024 * 1024,
-        },
-      }).fields([
-        { name: 'image', maxCount: 1 },
-        { name: 'media', maxCount: 1 },
-      ]);
+  //     const upload = multer({
+  //       storage: multerStorage,
+  //       fileFilter: multerFilter,
+  //       limits: {
+  //         fileSize: 100 * 1024 * 1024,
+  //       },
+  //     }).fields([
+  //       { name: 'image', maxCount: 1 },
+  //       { name: 'media', maxCount: 1 },
+  //     ]);
 
-      upload(req, res, async (err) => {
-        if (err) {
-          throw new BadRequestError(err.message);
-        }
+  //     upload(req, res, async (err) => {
+  //       if (err) {
+  //         throw new BadRequestError(err.message);
+  //       }
 
-        const {
-          callback,
-          attributes,
-        } = req.body;
+  //       const {
+  //         callback,
+  //         attributes,
+  //       } = req.body;
 
-        await validateIpfsUpload(req.body);
+  //       await validateIpfsUpload(req.body);
 
-        const image = new Image();
+  //       const image = new Image();
 
-        const imageFilename = req.files.image[0].filename;
+  //       const imageFilename = req.files.image[0].filename;
 
-        image.localPath = `${NFTFilesDest}/${imageFilename}`;
-        image.filename = imageFilename;
-        image.mimetype = path.extname(imageFilename);
-        image.callback = callback;
-        await image.save();
+  //       image.localPath = `${NFTFilesDest}/${imageFilename}`;
+  //       image.filename = imageFilename;
+  //       image.mimetype = path.extname(imageFilename);
+  //       image.callback = callback;
+  //       await image.save();
 
-        let media;
+  //       let media;
 
-        if (req.files.media) {
-          media = new Media();
+  //       if (req.files.media) {
+  //         media = new Media();
 
-          const mediaFilename = req.files.media[0].filename;
+  //         const mediaFilename = req.files.media[0].filename;
 
-          media.localPath = `${NFTFilesDest}/${mediaFilename}`;
-          media.filename = mediaFilename;
-          media.mimetype = path.extname(mediaFilename);
-          media.image = image.id;
-          await media.save();
+  //         media.localPath = `${NFTFilesDest}/${mediaFilename}`;
+  //         media.filename = mediaFilename;
+  //         media.mimetype = path.extname(mediaFilename);
+  //         media.image = image.id;
+  //         await media.save();
 
-          await addMediaJob(media.id);
-        } else {
-          await addImageJob(image.id);
-        }
+  //         await addMediaJob(media.id);
+  //       } else {
+  //         await addImageJob(image.id);
+  //       }
 
-        const nft = new NFTModel();
+  //       const nft = new NFTModel();
 
-        nft.image = image.id;
-        if (req.files.media) nft.media = media.id;
-        nft.metadata = {
-          name: req.body.name,
-          external_url: req.body.external_url,
-          description: req.body.description,
-          attributes,
-        };
+  //       nft.image = image.id;
+  //       if (req.files.media) nft.media = media.id;
+  //       nft.metadata = {
+  //         name: req.body.name,
+  //         external_url: req.body.external_url,
+  //         description: req.body.description,
+  //         attributes,
+  //       };
 
-        await nft.save();
+  //       await nft.save();
 
-        res.send({
-          message: 'Upload pending',
-          nftId: nft.id,
-        });
-      });
-    } catch (e) {
-      next(e);
-    }
-  },
+  //       res.send({
+  //         message: 'Upload pending',
+  //         nftId: nft.id,
+  //       });
+  //     });
+  //   } catch (e) {
+  //     next(e);
+  //   }
+  // },
 };
--- server/routes/mint.router.js
+++ server/routes/mint.router.js
@@ -4,5 +4,5 @@ const mintController = require('../controllers/mint.controller');
 const router = express.Router();
 
 router.post('/721/:nftId', mintController.PostMint721);
-
+// todo: add retry api
 module.exports = router;
