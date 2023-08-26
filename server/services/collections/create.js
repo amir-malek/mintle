d6719be65cfab0b16eeb4a9ea2038761ad14734c
Amir Malek <ahmalekpour1999@gmail.com>
2023-08-26 12:35:35 +0330

    bug fixes

--- server/services/collections/create.js
+++ server/services/collections/create.js
@@ -4,7 +4,7 @@ const config = require('config');
 const contract = require('../../../artifacts/server/contracts/Mint721.sol/MintNFT721.json');
 
 module.exports = async (to, uri) => {
-  const web3 = new Web3(config.get('web3.alchemy.sepolia.apiUrl'));
+  const web3 = new Web3(config.get('web3.alchemy.polygon.apiUrl'));
 
   const networkId = await web3.eth.net.getId();
   const myContract = new web3.eth.Contract(
